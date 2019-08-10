e2de0143de0e13010bb82d26ea23ac50f109650f
Tyrion Austin <tyrion.austin@ufl.edu>
2019-08-10 03:59:26 -0500

    Fix background image, and change functions for swapping layers

--- js/main.js
+++ js/main.js
@@ -20,16 +20,33 @@ function loopThrough() {
 // The Event Listener
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxes[i].addEventListener('click', loopThrough);
+
+  myBoxes[i].addEventListener("click", changeLayer2);
 };
 
-//Hide first fortune teller, show second after 14 seconds(longest length)
-setTimeout(function(){
-    teller1.style.display = 'none';
-}, 14000);
+//Hide first fortune teller
+function changeLayer2() {
 
-setTimeout(function(){
+	const firstLayer = document.getElementById('colors');			// Determine current layer
+	const secondLayer = document.getElementById('numbers');	// Determine next layer to show
+	let count;											// Set empty count variable
+
+	// Add hide class to current layer and display block next layer
+	const swap = () => {
+    firstLayer.classList.add("hide");
     teller2.style.display = 'block';
-}, 14000);
+	}  // End swap function
+
+  // Checks for first or second layer
+  if (firstLayer === teller1) {
+    count = this.textContent.length;		// If firstLAyer, set count value text content length
+  } else {
+    count = Number(this.textContent);							// Else, set count value the number of text content
+  }  // End if/else
+
+	// Set timeout to swap layers
+	setTimeout(swap, count * 2 * 1000);
+}  // End changeLayer2() function
 
 
 // Second function for loop based on number chosen.
@@ -53,27 +70,28 @@ for (let i = 0; i < theBoxes.length; i++) {
 
 function changeLayer() {
 
-	const currentLayer = document.getElementById('numbers');			// Determine current layer
+	const secondLayer = document.getElementById('numbers');			// Determine current layer
 	const nextLayer = document.getElementById('numbers2');	// Determine next layer to show
 	let count;											// Set empty count variable
 
-	// Add hide class to current layer and remove from next layer
+	// Add display none to fortune teller 2 remove hide from third fortune teller
 	const swap = () => {
     teller2.style.display = 'none';
     nextLayer.classList.remove("hide");
 	}  // End swap function
 
-  // Check to see if current layer is layer 1 or layer 2 to accurately set count value
-  if (currentLayer === teller2) {
-    count = Number(this.textContent);		// If layer1, set count value to image alt attribute
+  // Check to see if current layer is layer 2 or layer 3 to accurately set count value
+  if (secondLayer === teller2) {
+    count = Number(this.textContent);		// If secondLayer, set count value to number in text content
   } else {
-    count = Number(this.textContent);							// Else, set count value to square's text content
+    count = Number(this.textContent);							// Else, set count value to number in text content
   }  // End if/else
 
-	// Set timeout to swap layers
+	// Set timeout for layer swap
 	setTimeout(swap, count * 2 * 1000);
 }  // End changeLayer() function
 
+// Fortunes section
 
 let yourFortune = document.createElement('p');
 let fortuneWrapper = document.getElementById('fortunes');
