1475a0da167b41d44a1990dcccfdc0cd41f7ad68
Tyrion Austin <tyrion.austin@ufl.edu>
2019-08-09 20:53:14 -0500

    Add background image and code for second loop

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // Global Variables
 const myBoxes = document.getElementsByClassName('box');
+const theBoxes = document.getElementsByClassName('box2')
+const teller1 = document.getElementById('colors')
+const teller2 = document.getElementById('numbers')
 
 // The Functionality
 function loopThrough() {
@@ -15,4 +18,27 @@ function loopThrough() {
 // The Event Listener
 for (let i = 0; i < myBoxes.length; i++) {
   myBoxes[i].addEventListener('click', loopThrough);
+};
+
+setTimeout(function(){
+    teller1.style.display = 'none';
+}, 14000);
+
+setTimeout(function(){
+    teller2.style.display = 'block';
+}, 14000);
+
+
+// Second function for loop based on number chosen.
+function loop2() {
+
+  const changeColor2 = () => this.classList.toggle('new-color');
+
+  for(let i = 0); i < this.textContent.length * 2; i++) {
+    setTimeout(changeColor2, i * 1000);
+  };
+};
+// Event listener for second loop
+for (let i = 0; i < theBoxes.length; i++) {
+  theBoxes[i].addEventListener('click', loop2);
 };
